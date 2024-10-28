08a7ba0361d09122be8ed5cd26a73c0302cf8713
lblok <sendtolucy@gmail.com>
2024-10-28 10:08:32 -0400

    still didnt work

--- redirect.js
+++
@@ -1,5 +0,0 @@
-// Check if the URL includes 'learning_R'
-if (window.location.pathname.includes("learning_R")) {
-    // Redirect to the main tutorials page
-    window.location.href = "/urpl_1620_tutorials/";
-}
